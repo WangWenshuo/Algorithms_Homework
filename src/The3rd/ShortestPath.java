7c6aa014a7fdfff9c5cb4513b574fa64eb73e32d
WangWenshuo <wangwenshuoxidian@gmail.com>
2016-12-29 01:06:54 +0800

    two

--- src/The3rd/ShortestPath.java
+++ src/The3rd/ShortestPath.java
@@ -1,17 +1,29 @@
 package The3rd;
 
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 public class ShortestPath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		In in = new In("/Users/Administrator/Desktop/Algorithms_Homework/src/The3rd/data.txt");
+		In in = new In("/Users/Administrator/Desktop/Algorithms_Homework/src/The3rd/usa.txt");
 		//System.out.println(args[0]);
 		//System.out.println(s);
 		Graph G = new Graph(in);
-		int x = in.readInt();
-		int y = in.readInt();
+		Iterable<Edge> edges=G.getEdges();
+		for (Edge edge : edges) {
+			Point point1 = edge.getEither();
+			double x0 = point1.getX();
+			double y0 = point1.getY();
+			Point point2 = edge.getOther(point1);
+			double x1 = point2.getX();
+			double y1 = point2.getY();
+			StdDraw.line(x0, y0, x1, y1);
+		}
+		int x = 1;//in.readInt();
+		int y = 70;//in.readInt();
 		int s=x;
 		Dijkstra dijkstra = new Dijkstra(G, Point.getPoint(s, G.getPoints()));
 		StdOut.print(s+" to "+y);
--- two.md
+++ two.md
@@ -7,6 +7,49 @@ grammar_cjkRuby: True
 ### 一、题目描述
 实现各种排序算法并且分析性能
 ### 二、解决方法算法和描述
+#### 0.各个排序算法需要实现的接口
+```
+interface Sort {
+	public abstract void sort(Comparable[] a);
+	//比较两个数据的大小，若第一个数据比第二个数据小则返回真，否则就返回假
+	public static boolean less(Comparable u,Comparable v){
+		return u.compareTo(v)<0;
+	}
+	//交换数组中某两个元素的值
+	public static void exch(Comparable a[],int i,int j){
+		Comparable t = a[i];
+		a[i]=a[j];
+		a[j]=t;
+	}
+	//打印出某个数组的各个元素的值
+	public static void show(Comparable a[]){
+		int N = a.length;
+		for(int i=0;i<N;i++){
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+	}
+	//判断数组是否有序
+	public static boolean isSorted(Comparable a[]){
+		int N = a.length;
+		for(int i=1;i<N;i++){
+			if(less(a[i], a[i-1])){
+				return false;
+			}
+		}
+		return true;
+	}
+	//判断数组的某部分是否有序
+	public static boolean isSorted(Comparable a[],int lo,int hi){
+		for(int i=lo;i<hi;i++){
+			if(less(a[i+1], a[i])){
+				return false;
+			}
+		}
+		return true;
+	}
+}
+```
 #### 1.插入排序
 #### 2.归并排序(递归排序和自底向上)
 #### 3.快排
