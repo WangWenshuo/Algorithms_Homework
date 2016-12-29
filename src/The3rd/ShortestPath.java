eb72eacfc93fee3b1941472dbbda159548aadbf1
WangWenshuo <wangwenshuoxidian@gmail.com>
2016-12-29 21:29:33 +0800

    asda

--- src/The3rd/ShortestPath.java
+++ src/The3rd/ShortestPath.java
@@ -8,22 +8,25 @@ import edu.princeton.cs.algs4.StdOut;
 public class ShortestPath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		In in = new In("/Users/Administrator/Desktop/Algorithms_Homework/src/The3rd/usa.txt");
+		In in = new In("/Users/Administrator/Desktop/Algorithms_Homework/src/The3rd/data.txt");
 		//System.out.println(args[0]);
 		//System.out.println(s);
 		Graph G = new Graph(in);
 		Iterable<Edge> edges=G.getEdges();
-		for (Edge edge : edges) {
+/*		for (Edge edge : edges) {
 			Point point1 = edge.getEither();
 			double x0 = point1.getX();
 			double y0 = point1.getY();
 			Point point2 = edge.getOther(point1);
 			double x1 = point2.getX();
 			double y1 = point2.getY();
+			StdDraw.setXscale(0,10000);
+			StdDraw.setYscale(0,10000);
+			StdDraw.setPenColor(StdDraw.BLACK);
 			StdDraw.line(x0, y0, x1, y1);
-		}
-		int x = 1;//in.readInt();
-		int y = 70;//in.readInt();
+		}*/
+		int x = 0;//in.readInt();
+		int y = 5;//in.readInt();
 		int s=x;
 		Dijkstra dijkstra = new Dijkstra(G, Point.getPoint(s, G.getPoints()));
 		StdOut.print(s+" to "+y);
--- src/The3rd/TestClient.java
+++ src/The3rd/TestClient.java
@@ -1,26 +1,12 @@
 package The3rd;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 public class TestClient {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		In in = new In("/Users/Administrator/Desktop/Algorithms_Homework/src/The3rd/data.txt");
-		//System.out.println(args[0]);
-		int s = Integer.parseInt(args[1]);
-		//System.out.println(s);
-		Graph G = new Graph(in);
-		int x = in.readInt();
-		int y = in.readInt();
-		Dijkstra dijkstra = new Dijkstra(G, Point.getPoint(s, G.getPoints()));
-		for(int t=0;t<G.getV();t++){
-			StdOut.print(s+" to "+t);
-			StdOut.printf(" (%4.2f): ", dijkstra.distTo(t));
-			if(dijkstra.hasPathTo(t))
-				for(Edge e:dijkstra.pathTo(t))
-					StdOut.print(e+"  ");
-			StdOut.println();
-		}
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(0, 0, 1000, 1000);
 	}
 }
